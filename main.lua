bd31ab7f76e790abee89f77f460c2a046df6cdc5
Atlanta Scripts <118575044+atlantascripts@users.noreply.github.com>
2024-07-12 22:30:13 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,13 +8,13 @@
 
 -- DISCORD SERVER: https://discord.gg/sCMKeZHxS5
 
-Citizen.CreateThread(function()
-	Citizen.Wait(1000)
+CreateThread(function()
+	Wait(1000)
 	while true do
-		Citizen.Wait(1000)
 		if IsPedArmed(PlayerPedId(), 6) then
 			SetWeaponRecoilShakeAmplitude(GetSelectedPedWeapon(PlayerPedId()), 0)
 			SetPedAccuracy(PlayerPedId(), 100)
 		end
+		Wait(1000)
 	end
 end) 
